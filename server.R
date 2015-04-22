0d0d354c95c3ce3c0a71e56ca591baf628caf232
Iga Korneta <iga.korneta@gmail.com>
2015-04-22 11:20:20 +0200

    Changing the app files
    Apparently I updated the app after Wikipedia updated its layout, but
    forgot to update the files in the repository. Whoops.

--- server.R
+++ server.R
@@ -52,11 +52,23 @@ shinyServer(
     #unfortunately, it is impossible to automatically obtain column names & hence countries
     in.data <- reactive({
       invalidateLater(1000*60*60*24, session)
-      dat <- readHTMLTable(url)[[2]]
-      if (length(colnames(dat))<11) dat <- readHTMLTable(url)[[5]]
-      dat <- dat[2:nrow(dat),c(1, 4:(ncol(dat)-1))]
-      colnames(dat) <- c("Date", "Guinea.cases", "Guinea.deaths", "Liberia.cases", "Liberia.deaths",
+      #dat <- readHTMLTable(url)[[2]]
+      #if (length(colnames(dat))<11) dat <- readHTMLTable(url)[[6]]
+      #dat <- dat[2:nrow(dat),c(1, 4:(ncol(dat)-1))]
+      dat1 <- readHTMLTable(url)[[5]]
+      dat1 <- dat1[2:nrow(dat1),c(1, 4:9)]
+      dat2 <- readHTMLTable(url)[[6]]
+      dat2 <- dat2[2:nrow(dat2),c(1, 4:7)]
+      dat3 <- readHTMLTable(url)[[7]]
+      dat3 <- dat3[2:nrow(dat3),c(1, 4:13)]
+      dat12 <- merge(dat1, dat2, by.x="V1", by.y="V1")
+      #dat30 <- cbind(dat3, V12=rep(0, nrow(dat3)), V13=rep(0, nrow(dat3)))
+      dat30 <- dat3
+      colnames(dat12) <- c("Date", "Guinea.cases", "Guinea.deaths", "Liberia.cases", "Liberia.deaths",
                          "Sierra.Leone.cases", "Sierra.Leone.deaths", "Nigeria.cases", "Nigeria.deaths", "Senegal.cases", "Senegal.deaths")
+      colnames(dat30) <- c("Date", "Guinea.cases", "Guinea.deaths", "Liberia.cases", "Liberia.deaths",
+                           "Sierra.Leone.cases", "Sierra.Leone.deaths", "Nigeria.cases", "Nigeria.deaths", "Senegal.cases", "Senegal.deaths")
+      dat <- rbind(dat12, dat30)
       dat$Date <- helper(dat$Date)
       dat[,2:ncol(dat)] <- sapply(dat[,2:ncol(dat)], function(x) as.numeric(gsub(",","",x)))
       dat <- arrange(dat, Date)
@@ -147,8 +159,9 @@ shinyServer(
     
     
     output$cases.map <- renderPlot({
+      dat <- in.data()
       cum.new.switch.temp <- cum.new.switch()
-      limits <- c(0,2000)
+      limits <- c(0,ceiling(max(dat[,c(2,4,6,8,10)])/1000)*1000)
       if (cum.new.switch.temp=="new") {limits <- c(0,30)}
       ggplot(current.data(), aes(x = long, y = lat, group = group, fill=cases)) + 
         geom_polygon(size = 1) +  geom_path(color="dark green") + coord_equal() +
@@ -158,8 +171,9 @@ shinyServer(
 
     
     output$deaths.map <- renderPlot({
+      dat <- in.data()
       cum.new.switch.temp <- cum.new.switch()
-      limits <- c(0,1200)
+      limits <- c(0,(ceiling(max(dat[,c(3,5,7,9,11)])/1000))*1000)
       if (cum.new.switch.temp=="new") {limits <- c(0,30)}
       ggplot(current.data(), aes(x = long, y = lat, group = group, fill=deaths)) + 
         geom_polygon(size = 1) +  geom_path(color="dark green") + coord_equal() +
